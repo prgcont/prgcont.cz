580b144a82b88a509e6069165cf240d710b2b237
David Becvarik <dbecvari@redhat.com>
2019-04-29 15:20:41 +0200

    inlined javascript guarding telegram link

--- docs/js/custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-	$('a[href^="go-home-stupid-bot"]').each(function (index) {
-		oh = $(this).attr("href").replace("go-home-stupid-bot", "https://t.me/joinchat");
-		$(this).attr("href", oh);
-	});
-});
